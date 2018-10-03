fe63c77ec7069d58a98e617a5cac1529ddd25371
surajnepal34 <surajnepal34@gmail.com>
2018-10-02 19:15:06 -0600

    My First Commit

---
+++ SampleSpringBootApi/src/main/java/com/company/SampleSpringBootApi/SampleSpringBootApiApplication.java
@@ -0,0 +1,12 @@
+package com.company.SampleSpringBootApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootApiApplication.class, args);
+	}
+}
---
+++ SampleSpringBootApi/src/test/java/com/company/SampleSpringBootApi/SampleSpringBootApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.SampleSpringBootApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleSpringBootApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
